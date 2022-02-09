38ca6f5698c5fb34f177998ec4d65d0ca73eb392
Igor Mandrik <creation@86@mail.ru>
2022-02-09 18:09:11 +0800

    Edited ProductManagerTest.java

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -15,9 +15,9 @@ class ProductManagerTest {
     static Product phone1 = new Smartphone(1, "Samsung Galaxy S22", 100000, "Samsung");
     static Product phone2 = new Smartphone(2, "Iphone 13", 110000, "Apple");
     static Product phone3 = new Smartphone(3, "Huawei P50 Pro", 80000, "Huawei");
-    static Product book1 = new Book(1, "Transhumanism inc", 800, "Victor Pelevin");
-    static Product book2 = new Book(2, "The ritual", 600, "Adam Nevill");
-    static Product book3 = new Book(3, "Lost Echoes", 700, "Joe Lansdale");
+    static Product book1 = new Book(4, "Transhumanism inc", 800, "Victor Pelevin");
+    static Product book2 = new Book(5, "The ritual", 600, "Adam Nevill");
+    static Product book3 = new Book(6, "Lost Echoes", 700, "Joe Lansdale");
 
     @BeforeAll
     static void setUp() {
